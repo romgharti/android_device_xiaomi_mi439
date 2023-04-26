aadabb11c2b9c981caef804afce06a1da1ea318d
Jprimero15 <jprimero155@gmail.com>
2023-04-26 15:09:08 +0800

    mi439: Build ParanoidDoze
    * use MITHORIUM_PRODUCT_PACKAGES
    * proximity only

--- device.mk
+++ device.mk
@@ -266,6 +266,13 @@ MITHORIUM_PRODUCT_PACKAGES += \
     GrapheneFrameworks
 endif
 
+# ParanoidDoze
+MITHORIUM_PRODUCT_PACKAGES += \
+    ParanoidDoze
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.sensor.proximity=true
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
